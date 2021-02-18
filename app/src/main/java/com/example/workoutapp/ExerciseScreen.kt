dca6b85cc00a672eea37f6b7634c5c3dbf86202b
nidhi11choudhary <nidhi11choudhary@gmail.com>
2021-02-18 15:08:23 -0500

    First Screen wihh start button

---
+++ app/src/main/java/com/example/workoutapp/ExerciseScreen.kt
@@ -0,0 +1,11 @@
+package com.example.workoutapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ExerciseScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_exercise_screen)
+    }
+}
